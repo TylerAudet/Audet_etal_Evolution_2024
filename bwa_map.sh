e71b610405ac7c91b8e9a5d04aca33e18fbdc454
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2021-06-02 11:40:55 -0400

    Update bwa_map.sh

--- bwa_map.sh
+++ bwa_map.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -t 8:00:00 # I am unsure if that is how long it will take
+#SBATCH -t 4:00:00 # I am unsure if that is how long it will take
 #SBATCH -A def-idworkin
-#SBATCH --array=0-57
+#SBATCH --array=0-32
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=20G
+#SBATCH --mem=6G
 #SBATCH --mail-user=audett@mcmaster.ca
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -12,7 +12,6 @@
 module load bwa/0.7.17
 
 # make the common path available in variable (this is optional, and just for the looks
-
 BASE=/home/audett/projects/def-idworkin/audett/SSD
 
 # make it arrays so you can index them
@@ -25,7 +24,7 @@ R1=${forward[${SLURM_ARRAY_TASK_ID}]}
 prefix=${R1%%_R1_trimmed.fastq}
  
 bwa mem -t ${SLURM_CPUS_PER_TASK} -M ${BASE}/ref/dmel-all-chromosome-r6.23.fasta.gz ${prefix}_R1_trimmed.fastq \
-   ${prefix}_R2_trimmed.fastq > ${prefix}.sam
+   ${prefix}_R2_trimmed.fastq > ${BASE}/mapped/${prefix}.sam
 
 # -M Mark shorter split hits as secondary (for Picard compatibility).
 # -t used to tell BWA to use 32 threads to speed things up
