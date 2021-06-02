614e979f6c149f5301f0edb5781c76c41b98fe29
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2021-06-02 14:25:49 -0400

    Update bbduk.sh

--- bbduk.sh
+++ bbduk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -t 4:00:00
 #SBATCH -A def-idworkin
+#SBATCH --array=0-57
 #SBATCH -n 32
 #SBATCH --mem=10G
 #SBATCH --mail-user=audett@mcmaster.ca
@@ -8,24 +9,25 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-raw_dir=/home/audett/projects/def-idworkin/audett/SSD/genomes
+module load bbmap/38.86
 
-trim_dir=/home/audett/projects/def-idworkin/audett/SSD/trimmed
+in=/home/audett/projects/def-idworkin/audett/SSD/genomes
+out=/home/audett/projects/def-idworkin/audett/SSD/trimmed
 
-files=(${raw_dir}/*_R1.fastq)
+declare -a forward=( ${in}/*_R1.fastq )
 
-for file in ${files[@]}
-do
-name=${file}
+R1=${forward[${SLURM_ARRAY_TASK_ID}]}
+
+name=${R1}
 base=`basename ${name} _R1.fastq`
 
 bbduk.sh \
-in1=${raw_dir}/${base}_R1.fastq \
-in2=${raw_dir}/${base}_R2.fastq \
-out1=${trim_dir}/${base}_R1_trimmed.fastq \
-out2=${trim_dir}/${base}_R2_trimmed.fastq \
+in1=${in}/${base}_R1.fastq \
+in2=${in}/${base}_R2.fastq \
+out1=${out}/${base}_R1_trimmed.fastq \
+out2=${out}/${base}_R2_trimmed.fastq \
 ref=/home/audett/projects/def-idworkin/audett/SSD/trimmed
-threads=32 ftr=149 ktrim=r k=23 mink=6 hdist=1 tpe tbo \
+threads=32 ftr=149 ktrim=r k=23 mink=10 hdist=1 tpe tbo \
 qtrim=rl trimq=20 minlength=36 2> /home/audett/projects/def-idworkin/audett/SSD/trimmed/log
 
 done
