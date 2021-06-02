8642535f774516a269301fcb2c2d31ae2aa9a2a8
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2021-06-02 09:17:27 -0400

    Update bwa_map.sh

--- bwa_map.sh
+++ bwa_map.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
-#SBATCH -t 48:00:00
+#SBATCH -t 8:00:00 # I am unsure if that is how long it will take
 #SBATCH -A def-idworkin
-#SBATCH -n 32
-#SBATCH --mem-per-cpu=10G
+#SBATCH --array=0-57
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=20G
 #SBATCH --mail-user=audett@mcmaster.ca
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
+module load bwa/0.7.17
 
-# directory of processed sequences with BBduk
-trim_dir=/home/audett/projects/def-idworkin/audett/SSD/trimmed
+# make the common path available in variable (this is optional, and just for the looks
 
-# variable for the reference genome
-refGenome=/home/audett/projects/def-idworkin/audett/SSD/genomes/dmel-all-chromosome-r6.23.fasta.gz
+BASE=/home/audett/projects/def-idworkin/audett/SSD
 
-# make output directory from mapping outputs
-output=/home/audett/projects/def-idworkin/audett/SSD/mapped
+# make it arrays so you can index them
+declare -a forward=( ${BASE}/trimmed/*_R1_trimmed.fastq )
 
-#list all files to be read (this selects the left end from each PE pair)
-files=(${trim_dir}/*_R1_trimmed.fastq)
-
-#For loop over every file
-for file in ${files[@]}
-do
-name=${file}
-base=`basename ${name} _R1_trimmed.fastq`
-
-bwa mem -t 32 -M ${refGenome} \
-${trim_dir}/${base}_R1_trimmed.fastq \
-${trim_dir}/${base}_R2_trimmed.fastq \
-> ${output}/${base}_bwa_PE.SAM
-done
+# get the individual reads.
+R1=${forward[${SLURM_ARRAY_TASK_ID}]}
+ 
+# get the prefix of the reads
+prefix=${R1%%_R1_trimmed.fastq}
+ 
+bwa mem -t ${SLURM_CPUS_PER_TASK} -M ${BASE}/ref/dmel-all-chromosome-r6.23.fasta.gz ${prefix}_R1_trimmed.fastq \
+   ${prefix}_R2_trimmed.fastq > ${prefix}.sam
 
 # -M Mark shorter split hits as secondary (for Picard compatibility).
 # -t used to tell BWA to use 32 threads to speed things up
