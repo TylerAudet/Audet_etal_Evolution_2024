9934d7e401d22baaf57bb6e286130f4812f8946d
TylerAudet <77504755+TylerAudet@users.noreply.github.com>
2022-10-31 20:47:11 +0100

    Update CMH.r

--- CMH.r
+++ CMH.r
@@ -119,26 +119,39 @@ write.csv(data2, "/home/audett/scratch/SSD/Analysis/sexesMerged/CMH/LVS_pval.csv
 data <- na.omit(data)
 data2 <- na.omit(data2)
 
-
-
 data <- data[data$padj<quantile(data$padj,0.01),]
 data2 <- data2[data2$padj<quantile(data2$padj,0.01),]
-
-
 
 data$chromEnd <- data[,2]
 data2$chromEnd <- data2[,2]
 
-
 data <- data[,c(1,2,6)]
 data2 <- data2[,c(1,2,6)]
-
 
 headers <- c("chrom","chromStart","chromEnd")
 
 colnames(data)<-headers
 colnames(data2)<-headers
 
+data$temp <- paste("chr", data$chrom, sep = "")
+data2$temp <- paste("chr", data2$chrom, sep = "")
+
+data <- data[,c(4,2,3)]
+data2 <- data2[,c(4,2,3)]
+
+colnames(data) <- headers
+colnames(data2) <- headers
+
+data$temp <- data$chromStart - 5
+data$temp2 <- data$chromEnd + 5
+data <- data[c(1,4,5)]
+
+data2$temp <- data2$chromStart - 5
+data2$temp2 <- data2$chromEnd + 5
+data2 <- data2[c(1,4,5)]
+
+colnames(data) <- headers
+colnames(data2) <- headers
 
 write.table(data, file = "/home/audett/scratch/SSD/Analysis/sexesMerged/CMH/CVE_top1percent.bed", sep = "\t",
             row.names = FALSE, quote = FALSE)
